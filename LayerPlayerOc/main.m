b6eaf7f176e7135ede4dacdb7d892ae1f4cb100e
WymanY <wuyun768@icloud.com>
2015-08-17 11:48:19 +0800

    Initial Commit

---
+++ LayerPlayerOc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LayerPlayerOc
+//
+//  Created by 武蕴 on 15/8/17.
+//  Copyright (c) 2015年 WymanY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LayerPlayerOcTests/LayerPlayerOcTests.m
@@ -0,0 +1,40 @@
+//
+//  LayerPlayerOcTests.m
+//  LayerPlayerOcTests
+//
+//  Created by 武蕴 on 15/8/17.
+//  Copyright (c) 2015年 WymanY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LayerPlayerOcTests : XCTestCase
+
+@end
+
+@implementation LayerPlayerOcTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
