3e97e101acadbd15d32ca7955bdc50d115f33dba
wy3 <wy3@meitu.com>
2017-06-30 18:32:39 +0800

    [update]修改视图颜色

--- LayerPlayerOc/CALayerViewController.m
+++ LayerPlayerOc/CALayerViewController.m
@@ -23,12 +23,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     //setup Layer
     [self setUpLayer];
     [_viewForLayer.layer addSublayer:_layer];
     
     UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    redView.backgroundColor = [UIColor redColor];
     [self.view addSubview:redView];
+    
     
     
 }
